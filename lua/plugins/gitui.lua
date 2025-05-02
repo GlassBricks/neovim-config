f8dac5d3f203d11a8225c80d431504d2f183f353
Benjamin Ye <benjamin.ye@arista.com>
2025-05-01 20:40:33 -0700

    Add gitui fix

---
+++ lua/plugins/gitui.lua
@@ -0,0 +1,21 @@
+local gitui_fixed = "exec bash -c 'eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa && exec gitui'"
+
+return {
+  "williamboman/mason.nvim",
+  keys = {
+    {
+      "<leader>gG",
+      function()
+        Snacks.terminal(gitui_fixed)
+      end,
+      desc = "GitUi (cwd)",
+    },
+    {
+      "<leader>gg",
+      function()
+        Snacks.terminal(gitui_fixed, { cwd = LazyVim.root.get() })
+      end,
+      desc = "GitUi (Root Dir)",
+    },
+  },
+}
