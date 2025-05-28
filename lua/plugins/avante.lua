23f2fc4b8d533cadd69682a751cb0594d01f7cd4
Benjamin Ye <benjamin.ye@arista.com>
2025-05-28 11:36:27 -0700

    More stuff

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,82 @@
+if true then
+  return {}
+end
+
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false,
+    opts = {
+      provider = "gemini",
+      auto_suggestions_provider = "gemini",
+      selector = {
+        provider = "snacks",
+        provider_opts = {},
+      },
+      behavior = {
+        auto_suggestions = true,
+      },
+    },
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "echasnovski/mini.icons",
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    keys = {
+      { "<leader>a", group = "+ai" },
+      { "<leader>aa", "<cmd>AvanteAsk<cr>", desc = "ask" },
+      { "<leader>ar", "<cmd>AvanteRefresh<cr>", desc = "refresh" },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "Kaiser-Yang/blink-cmp-avante",
+    },
+    opts = {
+      sources = {
+        -- Add 'avante' to the list
+        default = { "avante", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          avante = {
+            module = "blink-cmp-avante",
+            name = "Avante",
+            opts = {
+              -- options for blink-cmp-avante
+            },
+          },
+        },
+      },
+    },
+  },
+}
---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,83 @@
+if false then
+  return {}
+end
+
+local adapter = vim.env["GEMINI_API_KEY"] and "gemini" or nil
+
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "ravitemer/mcphub.nvim",
+    },
+    opts = {
+      strategies = {
+        chat = { adapter = adapter },
+        inline = {
+          adapter = adapter,
+          keymaps = {
+            accept_change = {
+              modes = { n = "<leader>at" },
+              description = "Accept suggested change",
+            },
+            reject_change = {
+              modes = { n = "<leader>ar" },
+              description = "Reject suggested change",
+            },
+          },
+        },
+        cmd = { adapter = adapter },
+      },
+      display = {
+        diff = { enabled = true },
+      },
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
+          },
+        },
+      },
+    },
+
+    keys = {
+      { "<leader>a", "", desc = "Ai" },
+      { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", desc = "chat", mode = { "n", "v" } },
+      { "<leader>ai", "<cmd>CodeCompanion<cr>", desc = "inline", mode = { "n", "v" } },
+      { "<leader>am", "<cmd>CodeCompanionCmd<cr>", desc = "cmd", mode = { "n", "v" } },
+      { "<leader>ap", "<cmd>CodeCompanionActions<cr>", desc = "actions", mode = { "n", "v" } },
+      { "<leader>aa", "<cmd>CodeCompanionChat Add<cr>", desc = "add context", mode = { "n", "v" } },
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "codecompanion" },
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    opts = {
+      preview = {
+        filetypes = { "markdown", "codecompanion" },
+        ignore_buftypes = {},
+      },
+    },
+  },
+  {
+    "HakonHarnes/img-clip.nvim",
+    opts = {
+      filetypes = {
+        codecompanion = {
+          prompt_for_file_name = false,
+          template = "[Image]($FILE_PATH)",
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+}
